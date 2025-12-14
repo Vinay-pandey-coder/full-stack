aece4309abae4863ecd7e4fb8d8e737ac95c0d36
vinay pandey <abhi837688@gmail.com>
2025-12-14 07:19:18 +0530

    mongo db connect

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,55 @@
 const express = require("express")
 const cors = require('cors')
+const { MongoClient, ObjectId } = require("mongodb")
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-let users = []
+const url = 'mongodb+srv://abhi837688_db_user:pandey@cluster0.wyqtfhv.mongodb.net/?appName=Cluster0'
+const client = new MongoClient(url)
+
+let db;
 
 
-app.post('/add',(req,res)=>{
+async function connectDB(){
+  await client.connect()
+  console.log("mongodb connected")
+  db = client.db('formdb')
+}
+
+connectDB()
+
+
+app.post('/add',async(req,res)=>{
   let {name,password} = req.body
   if(name&&password){
-      users.push(req.body)
-  res.send({message:"data added successfuly"})
-  }else{
-    res.send({message:"enter your details"})
+      await db.collection('user').insertOne({
+        name,
+        password
+      })
+    res.send({ message: "data added successfully" })
+  } else {
+    res.send({ message: "enter your details" })
   }
 })
 
-app.get('/users/',(req,res)=>{
+app.delete('/delete/:id',async(req,res)=>{
+    const {id} = req.params
+    const result = db.collection('user').deleteOne({
+      _id:new ObjectId(id)
+    })
+    if (result.deletedCount === 1) {
+    res.send({ message: "data deleted successfully" })
+  } else {
+    res.send({ message: "data not found" })
+  }
+})
+
+
+app.get('/users/',async(req,res)=>{
+  const users = await db.collection('user').find().toArray()
   res.json(users)
 })
 
